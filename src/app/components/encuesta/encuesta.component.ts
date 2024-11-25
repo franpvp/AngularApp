66f09921e494c3d8615fe79e7330cd53fc53b3ae
franpvp <franpvp.98@gmail.com>
2024-11-25 20:53:38 -0300

    Se agregan validaciones a formulario encuesta

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -1,20 +1,39 @@
 import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-encuesta',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './encuesta.component.html',
   styleUrl: './encuesta.component.css'
 })
 export class EncuestaComponent {
 
   encuestaVisible: boolean = false;
+  encuestaForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Inicializamos el formulario con sus controles
+    this.encuestaForm = new FormGroup({
+      nombre: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      categoriaPreferencia: new FormControl('', Validators.required),
+      feedback: new FormControl(''),
+    });
+  }
 
   ngOnInit() {
     this.verificarEncuesta();
+    this.encuestaForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/), Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|cl)$/)]],
+      categoriaPreferencia: ['', Validators.required],
+      feedback: ['', Validators.required]
+      }
+    )
   }
 
   verificarEncuesta() {
@@ -27,10 +46,16 @@ export class EncuestaComponent {
     localStorage.setItem('encuestaMostrada', 'true');
   }
 
+  validarCampo(campo: string): boolean {
+    return !!this.encuestaForm.get(campo)?.invalid && 
+        (this.encuestaForm.get(campo)?.touched || false);
+  }
+
   onSubmit(formValue: any) {
     console.log('Datos enviados:', formValue);
     this.cerrarEncuesta();
   }
+  
 
 
 }
